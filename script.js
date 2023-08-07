80eac32e5b99814e7ac026d579b3e4ffe225cd00
Joshua Chu <jpchu@up.edu.ph>
2023-08-08 01:57:21 +0800

    change config files

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const DID_API = {
 };
 
 let userText = null;
-const API_KEY = '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'; // Paste your API key here
+const API_KEY = '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'; // Paste your API key here
 
 let peerConnection;
 let streamId;
